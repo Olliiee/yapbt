fab56afa1c8959d06fcbc03a412540719a7d4d28
Oliver Strauss <oliver.strauss@gmail.com>
2015-12-23 11:48:28 +0100

    minor updates for the GUI

--- Yapbt/YapbtEditor/MainWindow.cs
+++ Yapbt/YapbtEditor/MainWindow.cs
@@ -1,12 +1,18 @@
-﻿using System.Windows.Forms;
+﻿using System.IO;
+using System.Windows.Forms;
+using Org.Strausshome.Yapbt.Codes;
 
 namespace Org.Strausshome.Yapbt.YapbtEditor
 {
     public partial class MainWindow : Form
     {
+        private Objects.Fields fields = new Objects.Fields();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            fields.Config = new YapbtHandle.Configuration();
         }
 
         /// <summary>
@@ -17,7 +23,47 @@ namespace Org.Strausshome.Yapbt.YapbtEditor
         private void OpenAirport_Click(object sender, System.EventArgs e)
         {
             AirportSelection airportSelector = new AirportSelection();
-            airportSelector.ShowDialog();
+            if (airportSelector.ShowDialog(this) == DialogResult.OK)
+            {
+                bool addNewVariation = airportSelector.AddNewVariation;
+                ReturnCodes.FsVersion code = airportSelector.Code;
+                string icaoCode = airportSelector.IcaoCode;
+                string variatioName = airportSelector.VariatioName;
+
+                // Ok load the bgl file and covert it.
+                if (openBglXmlFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (!Directory.Exists(Directory.GetCurrentDirectory() + "\\temp"))
+                    {
+                        Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\temp");
+                    }
+
+                    DataReader.ReadBglData bglReader = new DataReader.ReadBglData();
+
+                    if (Path.GetExtension(openBglXmlFileDialog.FileName).ToLower() == ".xml")
+                    {
+                        if (bglReader.ConvertAndReadBgl(openBglXmlFileDialog.FileName) == ReturnCodes.Codes.Ok)
+                        {
+                            MessageBox.Show("Ok");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error");
+                        }
+                    }
+                    else if (Path.GetExtension(openBglXmlFileDialog.FileName).ToLower() == ".bgl")
+                    {
+                        if (bglReader.ConvertAndReadBgl(this.fields.Config.ReadConfig("bgltool"), openBglXmlFileDialog.FileName, "temp\temp.xml") == ReturnCodes.Codes.Error)
+                        {
+                            MessageBox.Show("Ok");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error");
+                        }
+                    }
+                }
+            }
         }
     }
 }
